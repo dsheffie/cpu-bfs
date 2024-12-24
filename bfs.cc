383676e10fbf7e34f3e7aff817949de0162e2386
David Sheffield <sheffield.david@gmail.com>
2024-12-24 09:21:46 -0800

    works but slow

--- bfs.cc
+++ bfs.cc
@@ -136,42 +136,25 @@ uint32_t bfs_avx512(uint32_t src, const graph *g) {
 	
 	/* load vertices */
 	__m512i v_vertices = _mm512_mask_loadu_epi32(_mm512_set1_epi32(0), k, &g->edges[s+j]);
-
 	
 	/* gather visited vertices */
-	__m512i v_visited = _mm512_mask_i32gather_epi32(_mm512_set1_epi32(0),
-							k,
-							v_vertices,
-							visited,
-							4);
+	__m512i v_visited = _mm512_mask_i32gather_epi32(_mm512_set1_epi32(0), k, v_vertices,
+							visited, 4);
 
 	/*  not visited positions */
 	__mmask16 kk = _mm512_cmp_epi32_mask(v_visited, _mm512_set1_epi32(0), _MM_CMPINT_EQ);
 	k = _kand_mask16(k, kk);
 	int nv = _mm512_mask2int(k);
-
+	int pc = __builtin_popcount(nv);
 	/* scatter back updated visited values */
 	_mm512_mask_i32scatter_epi32 (visited, k, v_vertices, _mm512_set1_epi32(1), 4);
-	
-	
-	int32_t punt[16] = {0};
-	_mm512_storeu_epi32(punt, v_vertices);
-
-	int p = next_cnt;
-	for(int z = 0; z < 16; z++) {
-	  int32_t v = punt[z];
-	  if(( (1<<z) & nv ) == 0)
-	    continue;
-	  //printf("vertex %d was not visited\n", v);
-	  
-	  next_frontier[p++] = v;
-	  assert(visited[v]);
-	}
 
 	/* generate offsets */
-	__m512i vc = _mm512_mask_compress_epi32(_mm512_set1_epi32(0), k, v_incr);
+	__m512i vc = _mm512_mask_compress_epi32(_mm512_set1_epi32(0), k, v_vertices);
+	k = _mm512_int2mask((1<<pc)-1);
+	_mm512_mask_storeu_epi32(&next_frontier[next_cnt], k, vc);
 
-	next_cnt += __builtin_popcount(nv);
+	next_cnt += pc;
 	vidx = _mm512_add_epi32(vidx, _mm512_set1_epi32(16));
       }
     }
